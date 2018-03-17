0f36c7f2558e4277b09ebb29291fa9904cb49d01
Alberto R.R. Manzanares <17321519+AlbertoRRM@users.noreply.github.com>
2018-03-17 14:37:20 +0100

    Lab 3 completed (unchecked)

--- lab3/lab3.vhd
+++ lab3/lab3.vhd
@@ -37,21 +37,21 @@ architecture syn of lab3 is
 
   signal clk, rst_n : std_logic;
   signal ready, rstInit_n : std_logic;
-  
+
   signal enterSync_n, enterDeb_n, enterFall : std_logic;
   signal switchesSync_n : std_logic_vector(7 downto 0);
   signal ldCode, eq, lock : std_logic;
   signal code : std_logic_vector(7 downto 0);
   signal tries : std_logic_vector(3 downto 0);
-    
+
 begin
 
   rstInit_n <= rstPb_n and ready;
-  
+
   resetSyncronizer : synchronizer
     generic map ( STAGES => 2, INIT => '0' )
     port map ( rst_n => rstInit_n, clk => clk, x => '1', xSync => rst_n );
-  
+
   clkGenerator : frequencySynthesizer
     generic map ( FREQ => 50_000, MODE => "LOW", MULTIPLY => 3, DIVIDE => 5 )
     port map ( clkIn => osc, ready => ready, clkOut => clk );
@@ -61,15 +61,15 @@ begin
   enterSynchronizer : synchronizer
     generic map ( STAGES => 2, INIT => '0' )
     port map ( rst_n => rst_n, clk => clk, x => enter_n, xSync => enterSync_n );
-   
+
   enterDebouncer : debouncer
     generic map ( FREQ => 30_000, BOUNCE => 50 )
     port map ( rst_n => rst_n, clk => clk, x_n => enterSync_n, xdeb_n => enterDeb_n );
-   
+
   enterEdgeDetector : edgeDetector
     port map ( rst_n => rst_n, clk => clk, x_n => enterDeb_n, xFall => enterFall, xRise => open );
-  
-  switchesSynchronizer : 
+
+  switchesSynchronizer :
   for i in switches_n'range generate
   begin
     switchsynchronizer : synchronizer
@@ -81,52 +81,71 @@ begin
 
   fsm:
   process (rst_n, clk, enterFall)
-    type states is (initial, S3, S2, S1, S0); 
+    type states is (initial, S3, S2, S1, S0);
     variable state: states;
-  begin 
-    if enterFall = HIGH then
+  begin
+    if enterFall = HIGH and state = initial then
       ldCode <= '1';
-    ...
+    else
+      ldCode <= '0';
+    if state = initial then
+      lock <= '0';
+    else
+      lock <= '1';
     end if;
     case state is
       when initial =>
-        tries <= "1010"; -- A 
-        lock  <= '0';
+        tries <= "1010"; -- A
 		when S3 =>
         tries <= "0011"; -- 3
-        lock  <= '1';
 		when S2 =>
         tries <= "0010"; -- 2
-        lock  <= '1';
 		when S1 =>
         tries <= "0001"; -- 1
-        lock  <= '1';
 		when S0 =>
         tries <= "1100"; -- C
-        lock  <= '1';
     end case;
     if rst_n='0' then
       state := initial;
     elsif rising_edge(clk) then
       case state is
         when initial =>
-        ...
+          if enterFall = '1' then
+            state <= S3;
+        when S3 =>
+          if enterFall = '1' and eq = '0' then
+            state <= S2;
+          elsif enterFall = '1' and eq = '1' then
+            state <= initial;
+        when S2 =>
+          if enterFall = '1' and eq = '0' then
+            state => S1;
+          elsif enterFall = '1' and eq = '1' then
+            state <= initial;
+        when S1 =>
+          if enterFall = '1' and eq = '0' then
+            state <= S0;
+          elsif enterFall = '1' and eq = '1' then
+            state = initial;
       end case;
     end if;
-  end process;  
+  end process;
 
   codeRegister :
   process (rst_n, clk)
   begin
-    ...
+    if rst_n='0' then
+      code <= (others=>'0');
+    elsif rising_edge(clk) then
+      code <= switchesSync_n;
   end process;
-  
+
   comparator:
-  eq <= ...;
-    
-  rigthConverter : bin2segs 
-    port map( ... );
-  
-  leds <= ...;
+  eq <= switchesSync_n and code;
+
+  rigthConverter : bin2segs
+    port map( bin => tries, dp => NO, segs => upSegs );
+
+  leds <= lock;
 
 end syn;
